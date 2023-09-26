ffafe2876e42317302e450967af1e90ad0e5e13c
Nikolay Bely <belyynikolay@gmail.com>
2023-09-26 22:23:11 +0700

    Test Vue2 integration
    Co-authored-by: DOXOPOKC <lilondin@gmail.com>

--- server/core/views.py
+++ server/core/views.py
@@ -1,5 +1,8 @@
 """Core app views."""
 
+import uuid
+from datetime import datetime, timedelta
+
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
@@ -13,4 +16,18 @@ def index(request: HttpRequest) -> HttpResponse:
     Returns:
         Rendered response
     """
-    return render(request, "core/hello_world.html")
+    context = {}
+    tmp = [
+        {
+            "id": i,
+            "key": str(uuid.uuid4()),
+            "time": str(datetime.now() + timedelta(minutes=i)),
+            "txt": "".join((str(i) * i)[:500]),
+        }
+        for i in range(1000)
+    ]
+
+    context["store"] = {}
+    context["store"]["todos"] = tmp
+
+    return render(request, "core/hello_world.html", context)
